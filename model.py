4f4d83b8c7edc4a32c229131b768c92ac3daa993
Mohamed Rachidi <rachidi29@gmail.com>
2018-12-04 17:50:25 -0800

    Update model.py
    http

--- model.py
+++ model.py
@@ -14,11 +14,11 @@ def nms(dets, thresh):
     return pth_nms(dets, thresh)
 
 model_urls = {
-    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+    'resnet18': 'http://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'http://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'http://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'http://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'http://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
 
 class PyramidFeatures(nn.Module):
