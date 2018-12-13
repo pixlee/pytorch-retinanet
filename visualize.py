a485431cb05fca8735df5257863415f6fd001dd0
Mohamed Rachidi <rachidi29@gmail.com>
2018-12-13 11:31:40 -0800

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -88,7 +88,7 @@ def main(args=None):
 				cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
 				print(label_name)
 
-			cv2.imwrite(f'{idx}.png',img)
+			cv2.imwrite(str(idx) +'.png',img)
 # 			cv2.imshow('img', img)
 # 			cv2.waitKey(0)
 
